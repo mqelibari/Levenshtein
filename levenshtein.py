1bdd5f1905740c4d3548266099e1cb6c94f558d4
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-13 14:44:35 +0200

    Add test for empty string and not empty string

--- levenshtein.py
+++ levenshtein.py
@@ -1,2 +1,3 @@
 def levenshtein_distance(word_one: str, word_two: str) -> int:
-    return 0
+    if(len(word_one) == 0) or (len(word_two) == 0):
+        return max(len(word_one), len(word_two))
--- test_levenshtein.py
+++ test_levenshtein.py
@@ -7,6 +7,10 @@ class LevenshteinDistanceTestCase(unittest.TestCase):
         result = levenshtein_distance("", "")
         self.assertEqual(0, result)
 
+    def test_distanceOfOneCharToEmptyStringEqualsOne(self):
+        result = levenshtein_distance("a", "")
+        self.assertEqual(1, result)
+
 
 if __name__ == '__main__':
     unittest.main()
