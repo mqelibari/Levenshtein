5081a47e42f044f0abb0b2b2462fc7d3bfc1d661
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-13 14:51:57 +0200

    Add test for removing spaces

--- levenshtein.py
+++ levenshtein.py
@@ -1,3 +1,5 @@
 def levenshtein_distance(word_one: str, word_two: str) -> int:
+    word_one = word_one.replace(" ", "")
+    word_two = word_two.replace(" ", "")
     if(len(word_one) == 0) or (len(word_two) == 0):
         return max(len(word_one), len(word_two))
--- test_levenshtein.py
+++ test_levenshtein.py
@@ -11,6 +11,10 @@ class LevenshteinDistanceTestCase(unittest.TestCase):
         result = levenshtein_distance("a", "")
         self.assertEqual(1, result)
 
+    def test_distanceOfSpacesToStringEqualsStringLength(self):
+        result = levenshtein_distance("     ", "avs")
+        self.assertEqual(3, result)
+
 
 if __name__ == '__main__':
     unittest.main()
