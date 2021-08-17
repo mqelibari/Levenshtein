ed7913bb59cc369de747c4771bd1a4af7f5f4b32
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-17 10:29:46 +0200

    Add test_levenshtein_with_one_empty_string

--- levenshtein_lib.c
+++ levenshtein_lib.c
@@ -1,6 +1,12 @@
 #include "levenshtein_lib.h"
+#include "string.h"
+
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 
 int levenshtein(char* word_one, char* word_two){
+	if(strlen(word_one) == 0 || strlen(word_two) == 0){
+		return MAX(strlen(word_one), strlen(word_two));
+	}
 	return 0;
 }
--- test_levenshtein.c
+++ test_levenshtein.c
@@ -4,15 +4,22 @@
 void setUp(){}
 void tearDown(){}
 
-void test_levenshtein_with_empty_strings(){
+void test_levenshtein_with_empty_strings(void){
 	int result = levenshtein("", "");
 	TEST_ASSERT_EQUAL_INT(0, result);
+}
+
+
+void test_levenshtein_with_one_empty_string(void){
+	int result = levenshtein("s", "");
+	TEST_ASSERT_EQUAL_INT(1, result);
 }
 
 
 int main(){
 	UNITY_BEGIN();
 	RUN_TEST(test_levenshtein_with_empty_strings);
+	RUN_TEST(test_levenshtein_with_one_empty_string);
 	return UNITY_END();
 }
 
